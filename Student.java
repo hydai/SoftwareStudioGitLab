16df18cbe5459d27013c18074c9fc575cc62ac97
yeosblue <yeosblue@gmail.com>
2013-12-10 00:13:54 +0800

    Create Student class

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+	private String name = "Tau-Heng Yeo";
+	private String studentID = "101065428";
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public String getStudentID() {
+		return this.studentID;
+	}
+
+}
