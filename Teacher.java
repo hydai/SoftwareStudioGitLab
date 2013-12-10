30c33f2e1ec908bb67a01083da28047cea704095
yeosblue <yeosblue@gmail.com>
2013-12-10 10:47:30 +0800

    Add courses to Teacher class

--- Teacher.java
+++ Teacher.java
@@ -1,7 +1,12 @@
 public class Teacher {
-	private String name = 'HC';
+	private String name = "HC";
+	private String[] courses = {"Software Studio", "HCI Research Method"};
 	
 	public getName() {
 		return this.name;
+	}
+
+	public getCourses() {
+		return this.courses;
 	}
 }
